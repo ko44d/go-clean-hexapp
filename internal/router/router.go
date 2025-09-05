18ebade929a74ea266e209de699f898f4b614a0f
ko44d <koyoshid622@gmail.com>
2025-09-05 23:46:47 +0900

    - 不要なコメントを削除 (NewRouter)

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,7 +6,6 @@ import (
 	"github.com/ko44d/go-clean-hexapp/internal/container"
 )
 
-// NewRouter sets up the HTTP routing using handlers from the DI container.
 func NewRouter(c *container.Container) http.Handler {
 	mux := http.NewServeMux()
 
