307bd830c15972aebb7377c41dcc8f244d738cb1
ko44d <koyoshid622@gmail.com>
2025-09-17 16:30:22 +0900

    - init.sqlのフォーマットを修正   - tasksテーブルで不要な空白を削除

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,12 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS tasks (
-                                     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     title TEXT NOT NULL,
     description TEXT,
     status TEXT NOT NULL CHECK (status IN ('todo', 'in_progress', 'done')),
-
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP NOT NULL DEFAULT now()
     );
